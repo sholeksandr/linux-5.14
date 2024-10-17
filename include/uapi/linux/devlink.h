48afab3d967c43d5578dceb3acd985994ddc7e4d
Vadim Pasternak <vadimp@nvidia.com>
2024-10-17 18:24:06 +0300

    devlink: introduce linecard info get message
    Allow the driver to provide per line card info get op to fill-up info,
    similar to the "devlink dev info".
    
    devlink: introduce line card devices support
    
    Line card can contain a device. For example, this can be a gearbox with
    flash. This flash could be updated. Provide the driver possibility to
    attach such devices to a line card and expose those to user. Leverage
    the existing devlink flash update mechanism and allow driver to pass a
    custom "component name" string identifying the line card device to
    flash.
    
    Signed-off-by: Jiri Pirko <jiri@nvidia.com>

--- include/uapi/linux/devlink.h
+++ include/uapi/linux/devlink.h
@@ -578,6 +578,10 @@ enum devlink_attr {
 	DEVLINK_ATTR_LINECARD_TYPE,		/* string */
 	DEVLINK_ATTR_LINECARD_SUPPORTED_TYPES,	/* nested */
 	DEVLINK_ATTR_LINECARD_INFO,		/* nested */
+	DEVLINK_ATTR_LINECARD_DEVICE_LIST,	/* nested */
+	DEVLINK_ATTR_LINECARD_DEVICE,		/* nested */
+	DEVLINK_ATTR_LINECARD_DEVICE_INDEX,	/* u32 */
+	DEVLINK_ATTR_LINECARD_DEVICE_INFO,	/* nested */
 
 	/* add new attributes above here, update the policy in devlink.c */
 
